76f8e004d1902e1782a1ec96c66b0d87806d2049
ksr <rlatjdfud125@naver.com>
2023-04-06 14:42:41 +0900

    0406

---
+++ src/chapter_05/Comment.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Comment from "./CommentList";
+
+function CommentList(props) {
+    return (
+        <div>
+            <Comment />
+        </div>
+    );
+}
+
+export default CommentList;
--- src/index.js
+++ src/index.js
@@ -7,12 +7,13 @@ import reportWebVitals from './reportWebVitals';
 
 import Library from './chapter_03/Library';
 import Clock from './chapter_04/Clock';
+import CommentList from './chapter_05/Comment';
 
 setInterval(() => {
 const root = ReactDOM.createRoot(document.getElementById('root'));
   root.render(
   <React.StrictMode>
-    <Clock/>
+    <CommentList/>
   </React.StrictMode>
 );
 }, 1000);
