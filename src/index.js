fc34d41fc17f138233890a30927995c25b1385e2
ksr <rlatjdfud125@naver.com>
2023-04-13 16:18:49 +0900

    230413

--- src/index.js
+++ src/index.js
@@ -8,12 +8,12 @@ import reportWebVitals from './reportWebVitals';
 import Library from './chapter_03/Library';
 import Clock from './chapter_04/Clock';
 import CommentList from './chapter_05/Comment';
-
+import Accommodate from './chapter_07/Accommodate';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
   root.render(
   <React.StrictMode>
-    <CommentList/>
+    <Accommodate/>
   </React.StrictMode>
 );
 
