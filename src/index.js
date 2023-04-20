536e43d8511dc8a75dd0739e3255d19010aae1dd
Kim <rlatjdfud125@naver.com>
2023-04-20 13:17:10 +0900

    0420

--- src/index.js
+++ src/index.js
@@ -9,11 +9,12 @@ import Library from './chapter_03/Library';
 import Clock from './chapter_04/Clock';
 import CommentList from './chapter_05/Comment';
 import Accommodate from './chapter_07/Accommodate';
+import Book from './chapter_03/Book';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
   root.render(
   <React.StrictMode>
-    <Accommodate/>
+    <CommentList/>
   </React.StrictMode>
 );
 
